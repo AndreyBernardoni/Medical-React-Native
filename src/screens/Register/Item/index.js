1339bd6dce4f4b4aa36b75e1a10eb141513d8829
Andrey Fialho <andrey@tiquetaque.com>
2024-07-18 23:45:10 -0300

    cleanup: removed unused importss and comments

--- src/screens/Register/Item/index.js
+++ src/screens/Register/Item/index.js
@@ -1,13 +1,10 @@
 import React, { memo } from 'react';
 
 import { UIBlock } from '../../../components/UI/Block';
-import { useNavigation, useTheme } from '@react-navigation/native';
+import { useTheme } from '@react-navigation/native';
 import { UIText } from '../../../components/UI/Text';
 import { UITextInput } from '../../../components/UI/TextInput';
-import { UIButton } from '../../../components/UI/Button';
-import { SCREENS } from '../../../constants/screens';
 import { UITabs } from '../../../components/UI/Tabs';
-import { UIIcon } from '../../../components/UI/Icon';
 
 const PeriodSelector = memo(({ onChange }) => {
   const [tab, setTab] = React.useState('elderly');
@@ -33,17 +30,9 @@ const PeriodSelector = memo(({ onChange }) => {
 
 export const Item = ({ index, values }) => {
   const { colors } = useTheme();
-  const { navigate } = useNavigation();
 
   const { setName, setLastName, setEmail, setPassword, setRole } = values.sets;
-  const { name, lastName, email, password, role } = values.variables;
-
-  // {
-  //   "email":"andrey@elderly.com",
-  //   "password":"261204",
-  //   "role":"elderly",
-  //   "name":"Roger Cuidador"
-  // }
+  const { name, lastName, email, password } = values.variables;
 
   return (
     <UIBlock tPadding="4xl" gap={32}>
